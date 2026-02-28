7bdd205b9979817a7ba24796afb8f6b2bdd80e22
rasetsukyo <jamesprisby@gmail.com>
2026-02-28 16:00:05 -0500

    fix: prevent startup crash with Firestore fallback when Firebase is unavailable

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,24 +1,58 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import '../models/collection_alert.dart';
 
 class FirestoreService {
-  final _db = FirebaseFirestore.instance;
+FirebaseFirestore? get _db =>
+Firebase.apps.isNotEmpty ? FirebaseFirestore.instance : null;
 
-  Stream<List<CollectionAlert>> streamCollections(String deviceId) {
-    return _db
-        .collection('users')
-        .doc(deviceId)
-        .collection('collections')
-        .snapshots()
-        .map((snap) => snap.docs.map((d) => CollectionAlert.fromMap(d.data())).toList());
-  }
+final Map<String, Map<String, CollectionAlert>> _local = {};
+final Map<String, StreamController<List<CollectionAlert>>> _controllers = {};
 
-  Future<void> upsertCollection(String deviceId, CollectionAlert collection) async {
-    await _db
-        .collection('users')
-        .doc(deviceId)
-        .collection('collections')
-        .doc(collection.slug)
-        .set(collection.toMap(), SetOptions(merge: true));
-  }
+StreamController<List<CollectionAlert>> _controllerFor(String deviceId) {
+return _controllers.putIfAbsent(
+deviceId,
+() => StreamController<List<CollectionAlert>>.broadcast(),
+);
+}
+
+Stream<List<CollectionAlert>> streamCollections(String deviceId) {
+final db = _db;
+if (db != null) {
+return db
+.collection('users')
+.doc(deviceId)
+.collection('collections')
+.snapshots()
+.map((snap) =>
+snap.docs.map((d) => CollectionAlert.fromMap(d.data())).toList());
+}
+
+final c = _controllerFor(deviceId);
+Future.microtask(() {
+final items = _local[deviceId]?.values.toList() ?? const <CollectionAlert>[];
+c.add(items);
+});
+return c.stream;
+}
+
+Future<void> upsertCollection(String deviceId, CollectionAlert collection) async {
+final db = _db;
+if (db != null) {
+await db
+.collection('users')
+.doc(deviceId)
+.collection('collections')
+.doc(collection.slug)
+.set(collection.toMap(), SetOptions(merge: true));
+return;
+}
+
+final userMap = _local.putIfAbsent(deviceId, () => {});
+userMap[collection.slug] = collection;
+_controllerFor(deviceId).add(userMap.values.toList());
+}
 }
